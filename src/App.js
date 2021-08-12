0163aa30f83f8f55f16a06ffe848244e531f04e9
HTT-vsc <57784066+HTT-vsc@users.noreply.github.com>
2021-08-12 16:56:13 +0700

    initial commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import { hot } from 'react-hot-loader'
+
+class App extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1>Hello World!!</h1>
+      </div>
+    )
+  }
+}
+
+// const hotFunction = hot(module)
+// export default hotFunction(App)
+
+export default hot(module)(App)
